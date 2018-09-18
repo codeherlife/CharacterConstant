148a779352c2d66285961c116ffd4259ae11a10f
codeherlife <codeherlife@gmail.com>
2018-09-18 13:05:15 -0700

    Added table for char values

--- main.c
+++ main.c
@@ -15,11 +15,41 @@
 FILE *fp;
 
 int main(void) {
-    //after variable declarations include the stmt fp = fopen("csis.txt", "w");
+    char backSlash = '\\';
+    char newlineChar = '\n';
+    char horizontalTab = '\t';
+    char verticalTab = '\v';
+    char backspace = '\b';
+    char carrReturn = '\r';
+    char formFeed = '\f';
+    char singleQuo = '\'';
+    char doubleQuo = '\"';
+    char nullConstant = '\0';
+
     fp = fopen("csis.txt", "w");
     
-    printf("Char Constant\tDescription\t\t\tValue\n");
-    //don't forget to have fprintf(fp, ); for every print statement
+    printf("%-3s %18s %10s \n", "Char Constant", "Description", "Value");
+    fprintf(fp, "%-3s %18s %10s \n", "Char Constant", "Description", "Value");
+    printf("\\%-3s %25s %10d \n", "n", "newline", newlineChar);
+    fprintf(fp, "\\%-3s %25s %10d \n", "n", "newline", newlineChar);
+    printf("\\%-3s %25s %10d \n", "t", "horizontal tab", horizontalTab);
+    fprintf(fp, "\\%-3s %25s %10d \n", "t", "horizontal tab", horizontalTab);
+    printf("\\%-3s %25s %10d \n", "v", "vertical tab", verticalTab);
+    fprintf(fp, "\\%-3s %25s %10d \n", "v", "vertical tab", verticalTab);
+    printf("\\%-3s %25s %10d \n", "b", "backspace", backspace);
+    fprintf(fp, "\\%-3s %25s %10d \n", "b", "backspace", backspace);
+    printf("\\%-3s %25s %10d \n", "r", "carriage return", carrReturn);
+    fprintf(fp, "\\%-3s %25s %10d \n", "r", "carriage return", carrReturn);
+    printf("\\%-3s %25s %10d \n", "f", "form feed", formFeed);
+    fprintf(fp, "\\%-3s %25s %10d \n", "f", "form feed", formFeed);
+    printf("\\%-3s %25s %10d \n", "\\", "backslash", backSlash);
+    fprintf(fp, "\\%-3s %25s %10d \n", "\\", "backslash", backSlash);
+    printf("\\%-3s %25s %10d \n", "'", "single quote", singleQuo);
+    fprintf(fp, "\\%-3s %25s %10d \n", "'", "single quote", singleQuo);
+    printf("\\%-3s %25s %10d \n", "\"", "double quote", doubleQuo);
+    fprintf(fp, "\\%-3s %25s %10d \n", "\"", "double quote", doubleQuo);
+    printf("\\%-3s %25s %10d \n", "0", "null", nullConstant);
+    fprintf(fp, "\\%-3s %25s %10d \n", "0", "null", nullConstant);
     
     fclose(fp);
     return 0;
